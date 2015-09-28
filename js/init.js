d6173cf75e57c8df670c498fe36d9f5773117fbc
Yacent <540608491@qq.com>
2015-09-28 18:25:37 +0800

    改变时间

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
 						   
 	$("#countdown").countdown({
-				date: "28 September 2015 18:26:00",
+				date: "28 September 2015 18:30:00",
 				format: "on"
 			},
 			
